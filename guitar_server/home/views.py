25bb08b8eb0c26851c09eefd149ee35c5d9fd8da
Alon Raizman <alon@startafire.com>
2016-09-15 16:52:57 +0300

    urls and templates configs

--- guitar_server/home/views.py
+++ guitar_server/home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+
+
+def index(request):
+    context = {}
+    # featuredProducts = FeaturedProduct.get_actives()
+    # context['featuredProducts'] = featuredProducts
+    template = loader.get_template('index.html')
+    import pdb
+    # pdb.set_trace()
+    return HttpResponse(template.render(context, request))
